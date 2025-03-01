6940839aaa2037010838d86c6dda5d3cfd452201
Lucas <169088896+void222222@users.noreply.github.com>
2025-03-01 11:11:20 -0300

    Add files via upload

---
+++ pastaJAVA/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+       int numero = rand.nextInt(101);
+       System.out.print("Digite seu numero: ");
+
+       String numeroDigitado = scanner.nextLine();
+        int tentativa = 0;
+
+
+        for ( numeroDigitado === numero ) {
+            System.out.print(" Certa resposta ");
+        }
+     if ( System.out.print(" voce errou "); )
+
+
+    }
+}
